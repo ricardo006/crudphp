1e0b72fe0c59f719e7abfe714643c54d30009ef9
Ricardo <rycardo.191@gmail.com>
2024-01-09 16:08:14 -0300

    header

--- views/header.php
+++ views/header.php
@@ -2,13 +2,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-    <title>N/eva Ileva</title>
+    <title>N</title>
     <link rel="stylesheet" href="css/styles.css">
 </head>
 
 <nav class="navbar navbar-expand-lg bg-purple bg-body-tertiary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="index.php">N<span>/eva</span></a>
+        <a class="navbar-brand" href="index.php">N/<span></span></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
             aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
